048020ed7fd0f617fb4a1299836a204d40f5421f
renanberton <renan_berton@yahoo.com.br>
2023-08-09 17:20:37 -0300

    Feature: Add imgs e icones + Header Feito e Responsivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="box-logo">
+          <a href="/">
+            <img src={process.env.PUBLIC_URL + '/icons/logo.png'} alt="Icone da página" /> 
+          </a>
+          <a href="/">
+            <span>ALOHA <br/> SURF <br/> SCHOOL <br/></span>
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">AULAS</a>
+            </li>
+            <li>
+              <a href="/">FOTOS</a>
+            </li>
+            <li>
+              <a href="/">CONTATO</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
