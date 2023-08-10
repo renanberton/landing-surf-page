a75f067d472c661f5d1282cba3022e8a449d97ea
renanberton <renan_berton@yahoo.com.br>
2023-08-09 21:03:07 -0300

    Feature: Add os icons faltantes + Feito a Parte dos icones + Responsividade e ajustes em outros

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import './App.scss';
 import { Header } from './components/Header';
 import { Banner } from './components/Banner';
 import { Intro } from './components/Intro';
-import Carousel from 'react-bootstrap/Carousel';
+import { Galery } from './components/Galery';
+import { Icons } from './components/Icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Image from 'react-bootstrap/Image';
+
 
 function App() {
   return (
@@ -13,32 +13,8 @@ function App() {
       <Header />
       <Banner />
       <Intro />
-      <div className="carousel">
-        <h3>Galeria de Fotos</h3>
-        <Carousel data-bs-theme="dark">
-          <Carousel.Item>
-            <img className="d-block w-100" src={process.env.PUBLIC_URL + '/imgs/image5.jpg'} alt="Icone da página" /> 
-            <Carousel.Caption>
-              
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img className="d-block w-100" src={process.env.PUBLIC_URL + '/imgs/image6.jpg'} alt="Icone da página" /> 
-            <Carousel.Caption>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img className="d-block w-100" src={process.env.PUBLIC_URL + '/imgs/image9.jpg'} alt="Icone da página" /> 
-            <Carousel.Caption>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
+      <Galery />
+      <Icons />      
     </div>
   );
 }
